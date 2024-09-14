5cd12eb009c12c0d05471eb48cd81285f55202ef
Pingching55 <160999581+Pingching55@users.noreply.github.com>
2024-09-14 23:59:42 +0800

    Update ChatScreen.jsx

--- src/screens/ChatScreen.jsx
+++ src/screens/ChatScreen.jsx
@@ -1,63 +1,91 @@
-// src/ChatScreen.js
-import React, { useEffect, useState } from 'react';
-import { useTheme } from '../contexts/ThemeContext';
-import { createStyles } from '../styles/themeStyles';
-
-import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, ScrollView, View, Button } from 'react-native';
 import io from 'socket.io-client';
+import { useTheme } from '../contexts/ThemeContext';
 
-const socket = io('http://192.168.1.9:5000'); // Your Flask server address
+var socket = io('http://10.0.2.2:5000/chat', {
+    transports: ['websocket'],
+});
 
-export default function ChatScreen() {
-    const { colors, toggleTheme } = useTheme();
-    const styles = createStyles(colors);
-    const [messages, setMessages] = useState([]);
+const ChatScreen = () => {
+    const { colors } = useTheme();
+
+    const [name, setName] = useState('');
     const [message, setMessage] = useState('');
+    const [chatroom, setChatroom] = useState('');
 
     useEffect(() => {
-        socket.on('message', (message) => {
-            setMessages((prevMessages) => [...prevMessages, message]);
+        socket.on('connect', () => {
+            console.log(socket.id);
+            socket.emit('mobile_client_connected', { connected: true }, (response) => {
+                console.log(response);
+            });
+            ToastAndroid.show('Connected to server', ToastAndroid.LONG);
         });
 
-        return () => {
-            socket.off('message');
-        };
+        socket.on('connect_to_client', (data) => {
+            let greets = JSON.parse(data);
+            console.log(greets);
+        });
+
+        socket.on('error', (error) => {
+            ToastAndroid.show('Failed to connect to server', ToastAndroid.LONG);
+        });
+
+        socket.on('message_broadcast', (data) => {
+            console.log(data);
+            let messageBag = JSON.parse(data);
+            setChatroom(chatroom => chatroom + `Message from ${messageBag.sender} at ${messageBag.timestamp}: \n${messageBag.message}\n\n`);
+        });
     }, []);
 
-    const sendMessage = () => {
-        socket.emit('message', message);
-        setMessage('');
-    };
-
     return (
-        <View style={styles.container}>
-            <FlatList
-                data={messages}
-                renderItem={({ item }) => <Text style={styles.text}>{item}</Text>}
-                keyExtractor={(item, index) => index.toString()}
-            />
+        <View style={{ flex: 1, padding: 10, backgroundColor: colors.background }}>
             <TextInput
-                style={styles.text}
-                value={message}
-                onChangeText={setMessage}
-                placeholder="Type a message"
+                style={{
+                    borderColor: 'black',
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    color: colors.text,
+                    marginBottom: 10,
+                    paddingHorizontal: 8,
+                }}
+                placeholder="Enter name"
+                value={name}
+                onChangeText={setName}
+                selectTextOnFocus={true}
                 placeholderTextColor={colors.text}
             />
-            <Button title="Send" onPress={sendMessage} />
+            <ScrollView>
+                <Text style={{ color: colors.text }}>{chatroom}</Text>
+            </ScrollView>
+            <TextInput
+                style={{
+                    borderColor: 'black',
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    color: colors.text,
+                    marginBottom: 10,
+                    paddingHorizontal: 8,
+                }}
+                placeholder="Enter message"
+                value={message}
+                onChangeText={setMessage}
+                selectTextOnFocus={true}
+                placeholderTextColor={colors.text}
+            />
+            <Button
+                title="Send"
+                onPress={() => {
+                    socket.emit('message_sent', {
+                        sender: name,
+                        message: message,
+                    });
+                }}
+            />
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 10,
-    },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 10,
-        paddingHorizontal: 8,
-    },
-});
+export default ChatScreen;
+
